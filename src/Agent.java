097adc26026d7b4e3c5021b9cee5c00128252465
fire9frenzy <tydeboon@gmail.com>
2015-03-20 14:48:05 -0600

    Agents and Location

--- src/Agent.java
+++ src/Agent.java
@@ -1,7 +1,40 @@
 public class Agent
 {
+	private Location target = null;
+	private Location position = null;
+	private String type = "C";
 	public Agent()
 	{
-		
+
+	}
+	public void update()
+	{
+
+	}
+	// when an agent first enters a room a target is set for the agent to path to
+	// this target is the location of the next door it needs to use
+	public void setTarget(Location t)
+	{
+		target = t;
+	}
+	public Location move()
+	{
+		return null;
+	}
+	public void updatePosition(Location p)
+	{
+		position = p;
+	}
+	public void ceatePath()
+	{
+
+	}
+	public String toString()
+	{
+		return "Agent: \n target: " + target + "\n position: " + position;
+	}
+	public String getType()
+	{
+		return type;
 	}
 }
