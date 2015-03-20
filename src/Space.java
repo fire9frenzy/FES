b1c2d4cb34c83919d1b75404ae625fba05dbe71e
bryanalfsib <bryan_alfsib@hotmail.com>
2015-03-20 14:46:48 -0600

    changes in Simulation, Space and Room

--- src/Space.java
+++ src/Space.java
@@ -6,10 +6,31 @@ public class Space
 	//2 -> floor
 	//3 -> door
  	private int type = 0;
-
+ 	private int dimension = 1;
+ 	Agent agent = null;
+ 	//dimension 1x1 meters
  	public Space(int type)
  	{
  		this.type = type;
+ 	}
+
+ 	public void setAgent(Agent agent)
+ 	{
+ 		this.agent = agent;
+ 	}
+
+ 	public Agent getAgent()
+ 	{
+ 		return agent;
+ 	}
+
+ 	public boolean hasAgent()
+ 	{
+ 		if(agent == null)
+ 		{
+ 			return false;
+ 		}
+ 		return true;
  	}
 
  	public void setType(int type)
@@ -21,5 +42,29 @@ public class Space
  	{
  		return type;
  	}
+
+ 	public String toString()
+ 	{
+ 		if(agent != null)
+ 		{
+ 			return "A";
+ 		}
+ 		if(type == 0)
+ 		{
+ 			return "W";
+ 		}
+ 		else if(type == 1)
+ 		{
+ 			return "O";
+ 		}
+ 		else if(type == 3)
+ 		{
+ 			return "D";
+ 		}
+
+ 		return " ";
+ 	}
+
+
 
 }
